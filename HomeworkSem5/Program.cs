12b9cc903a00f9f74b4f66fc75da0a4f3bfb1490
Alex <alexandrsorokin2@mail.ru>
2023-08-05 14:55:33 +0300

    Сделали первую задачу 5 ДЗ

---
+++ HomeworkSem5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 34: Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите программу,
+//  которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[10];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + " ");
+
+    if (array[i] / 2 * 2 < array[i])
+    {
+        count = count + 0;
+    }
+    else
+    {
+        count++;
+    }
+}
+System.Console.WriteLine(", Четных чисел " + count);
